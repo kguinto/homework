7072b3c3e3b1c1b7f5479d47f809a0141efe0358
kguinto <klogold1@gmail.com>
2018-05-07 13:35:43 -0700

    Implemented functionality to handle pagination

--- src/Views/UserListView/PageSelect.jsx
+++ src/Views/UserListView/PageSelect.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const PageSelect = () => (
-  <div>
-    <button>&lt;</button>
-    <button>&gt;</button>
+const PageSelect = ({ viewNextPage, viewPrevPage, currentPage }) => (
+  <div className='page-select'>
+    <div className='left-arrow' onClick={viewPrevPage} style={{ visibility: (currentPage > 1) ? 'visible' : 'hidden' }}>&lt;</div>
+    <div className='right-arrow' onClick={viewNextPage}>&gt;</div>
   </div>
 );
 
