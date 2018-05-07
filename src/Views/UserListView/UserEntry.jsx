6bad426ae07abccad17548e3b92d5afc12f55d74
kguinto <klogold1@gmail.com>
2018-05-07 13:34:38 -0700

    Updated to use ProfileCircle component and have a right arrow

--- src/Views/UserListView/UserEntry.jsx
+++ src/Views/UserListView/UserEntry.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import ProfileCircle from '../../sharedComponents/ProfileCircle';
 
 const UserEntry = ({ user, viewUser }) => (
-  <div className='user-entry' onClick={(event) => { viewUser(user) }}>{user.first_name} {user.last_name}</div>
+  <div className='user-entry' onClick={(event) => { viewUser(user) }}>
+    <ProfileCircle />
+    <div className='user-name'>{user.first_name} {user.last_name}</div>
+    <div className='right-arrow'>&gt;</div>
+  </div>
 );
 
 export default UserEntry;
