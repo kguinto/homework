dc0a3782f3e969a438f99ebb908984e6b42b4f4e
kguinto <klogold1@gmail.com>
2018-05-07 00:02:24 -0700

    Moved fetching and storing of user data to UserListView

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {API_HOST} from './constants';
 import UserListView from './Views/UserListView/UserListView'
 import UserEntryView from './Views/UserEntryView/UserEntryView';
 
@@ -10,23 +9,13 @@ const VIEWS = Object.freeze({ USER_LIST: 0, USER_ENTRY: 1 });
 class App extends Component {
   constructor (props) {
     super(props);
-    this.state = { users: [], currentUser: {}, view: VIEWS.USER_LIST };
+    this.state = { currentUser: {}, view: VIEWS.USER_LIST };
 
     this.viewUser = this.viewUser.bind(this);
     this.viewUserList = this.viewUserList.bind(this);
   }
 
-  componentDidMount() {
-    fetch(`${API_HOST}/api/users/`)
-      .then((res) => res.json())
-      .then((users) => {
-        console.log(users);
-        this.setState({ users })
-      });
-  }
-
   viewUser(user) {
-    console.log(user)
     this.setState({ view: VIEWS.USER_ENTRY, currentUser: user });
   }
 
@@ -39,7 +28,7 @@ class App extends Component {
 
     switch (this.state.view) {
       case VIEWS.USER_LIST:
-        view = <UserListView users={this.state.users} viewUser={this.viewUser}/>;
+        view = <UserListView viewUser={this.viewUser}/>;
         break;
       case VIEWS.USER_ENTRY:
         view = <UserEntryView user={this.state.currentUser} viewUserList={this.viewUserList} />;
