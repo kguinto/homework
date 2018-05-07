72e728263878c194c73baa208b9c5b75346580e9
kguinto <klogold1@gmail.com>
2018-05-06 18:51:10 -0700

    Implemented basic app structure that can switch between views

--- src/App.js
+++ src/App.js
@@ -2,25 +2,53 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {API_HOST} from './constants';
+import UserListView from './Views/UserListView/UserListView'
+import UserEntryView from './Views/UserEntryView/UserEntryView';
+
+const VIEWS = Object.freeze({ USER_LIST: 0, USER_ENTRY: 1 });
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { users: [], currentUser: {}, view: VIEWS.USER_LIST };
+
+    this.viewUser = this.viewUser.bind(this);
+    this.viewUserList = this.viewUserList.bind(this);
+  }
 
   componentDidMount() {
     fetch(`${API_HOST}/api/users/`)
       .then((res) => res.json())
-      .then(console.log)
+      .then((users) => {
+        console.log(users);
+        this.setState({ users })
+      });
+  }
+
+  viewUser(user) {
+    console.log(user)
+    this.setState({ view: VIEWS.USER_ENTRY, currentUser: user });
+  }
+
+  viewUserList () {
+    this.setState({ view: VIEWS.USER_LIST, currentUser: {} });
   }
 
   render() {
+    let view;
+
+    switch (this.state.view) {
+      case VIEWS.USER_LIST:
+        view = <UserListView users={this.state.users} viewUser={this.viewUser}/>;
+        break;
+      case VIEWS.USER_ENTRY:
+        view = <UserEntryView user={this.state.currentUser} viewUserList={this.viewUserList} />;
+        break;
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {view}
       </div>
     );
   }
