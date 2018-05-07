c52962d3e908e0a7735dbef7151c13d41c73781b
kguinto <klogold1@gmail.com>
2018-05-06 19:21:44 -0700

    Updated route to accept page parameter and return the appropriate set of 10 users

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -6,7 +6,13 @@ const db = require('./dbConnection');
 const config = require('../db/config');
 
 router.get('/users/', (req, res) => {
-  db.all(`SELECT * FROM User LIMIT 10`, (err, data) => {
+  let whereClause = ``;
+
+  if (req.query.page && req.query.page > 0) {
+    whereClause = `WHERE User.id > ${(req.query.page - 1) * 10} `;
+  }
+  
+  db.all(`SELECT * FROM User ${whereClause}LIMIT 10`, (err, data) => {
     if (err) {
       res.status(400).json({message: 'Error'})
     }
