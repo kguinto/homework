da59763648153cee5637a96ec1cb5f30c40f2ad6
kguinto <klogold1@gmail.com>
2018-05-06 23:33:59 -0700

    Updated to accept query parameter for searching the database

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -6,20 +6,37 @@ const db = require('./dbConnection');
 const config = require('../db/config');
 
 router.get('/users/', (req, res) => {
-
-  let whereClause = ``;
   let pageSize = req.query.page_size || 10;
+  let whereClause = ``;
 
-  if (req.query.page && req.query.page > 0) {
-    whereClause = `WHERE User.id > ${(req.query.page - 1) * pageSize} `;
+  if (Object.keys(req.query).length > 0) {
+    let clauses = [];
+
+    if (req.query.page && req.query.page > 0) {
+      clauses.push(`User.id > ${(req.query.page - 1) * pageSize} `);
+    }
+
+    if (req.query.query) {
+      let keyword = req.query.query;
+      let properties = ['id', 'first_name', 'last_name', 'email', 'ip_address'];
+      clauses.push(`( ${properties
+        .map(property => `User.${property} LIKE '%${keyword}%'`)
+        .concat(`User.gender LIKE '${keyword}%'`)
+        .join(' OR ') } ) `);
+    }
+
+    whereClause = `WHERE ${clauses.join('AND ')}`
   }
+
+  console.log(`SELECT * FROM User ${whereClause}LIMIT ${pageSize}`);
 
   db.all(`SELECT * FROM User ${whereClause}LIMIT ${pageSize}`, (err, data) => {
     if (err) {
+      console.log(err);
       res.status(400).json({message: 'Error'})
     }
 
-    if (data.length === 0) {
+    if (data && data.length === 0) {
       res.status(404).send('null');
     } else {
       res.status(200).send(data);   
